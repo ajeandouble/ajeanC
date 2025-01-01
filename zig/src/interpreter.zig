2f1ded37fcb47cd44a8169bef2c645df05fa4dfd
ajeandouble <jean.adrien.mtpl@gmail.com>
2025-01-01 19:28:11 +0100

    wip interpreter func decl interpration - arena allocator recursive error

--- zig/src/interpreter.zig
+++ zig/src/interpreter.zig
@@ -7,31 +7,86 @@ const UnaryOp = @import("./ast_nodes.zig").UnaryOp;
 const Num = @import("./ast_nodes.zig").Num;
 const TokenType = @import("./tokens.zig").TokenType;
 const activeTag = std.meta.activeTag;
+const Token = @import("./tokens.zig").Token;
 
 const NotImplented = error{NotImplemented}.NotImplemented;
 const Error = error{ InterpretError, DuplicateFunctionDeclaration, MissingMainFunctionDeclaration, MismatchingBinOpTypes };
 
-const ResultType = enum { integer, string, err };
+const ResultType = enum { integer, string, err, void };
 
-const Result = union(ResultType) { integer: struct { val: i64 }, string: struct { val: []u8 }, err: struct { type: Error, msg: []u8 } };
+const Result = union(ResultType) { integer: struct { val: i64 }, string: struct { val: []u8 }, err: struct { type: Error, msg: []u8 }, void: struct {} };
+const _VariableType = enum {
+    integer,
+    float,
+    str,
+};
+const _Variable = struct { type: _VariableType, str: []u8, int: i64 };
+
+pub const StackFrame = struct {
+    const Self = @This();
+    locals: std.StringHashMap(Result),
+
+    pub fn init(allocator: std.mem.Allocator) !*Self {
+        dbg.print("\n", .{}, @src());
+        const stack_frame = try allocator.create(StackFrame);
+        const locals = std.StringHashMap(Result).init(allocator);
+        stack_frame.* = .{ .locals = locals };
+        return stack_frame;
+    }
+
+    pub fn deinit(self: *Self) void {
+        self.locals.deinit();
+    }
+};
 
 pub const Interpreter = struct {
     const Self = @This();
     arena: std.heap.ArenaAllocator,
+    stack: std.ArrayList(StackFrame),
     ast: *const AstNode.Program = undefined,
 
     pub fn init(ast: *AstNode.Program, allocator: std.mem.Allocator) !Self {
-        const arena = std.heap.ArenaAllocator.init(allocator);
-        return Self{
-            .ast = ast,
-            .arena = arena,
-        };
+        var arena = std.heap.ArenaAllocator.init(allocator);
+        return Self{ .ast = ast, .arena = arena, .stack = std.ArrayList(StackFrame).init(arena.allocator()) };
     }
 
+    pub fn deinit(self: *Self) void {
+        self.arena.deinit();
+    }
+
+    // Stack
+    pub fn pushStack(self: *Self) !void {
+        dbg.print("Stack initialized: capacity = {}, length = {}\n", .{ self.stack.capacity, self.stack.items.len }, @src());
+        const new_frame = StackFrame{ .locals = std.StringHashMap(Result).init(std.heap.HeapAllocator) };
+        try self.stack.append(new_frame);
+    }
+
+    pub fn popStack(self: *Self) !void {
+        // TODO: garbage collection!
+        try self.stack.pop();
+    }
+
+    // Node visiting
     fn visitInteger(self: *Self, num: *Num) i64 {
+        dbg.print("{}", .{num.value}, @src());
         _ = self;
         return num.value;
     }
+
+    // Interpretation of funtion body
+    fn visitFuncDecl(self: *Self, func_decl: *const AstNode.FunctionDecl) !Result {
+        dbg.print("{s}\n", .{func_decl.id}, @src());
+        try self.pushStack();
+        // for (func_decl.statements.items) |stmt| {
+        //     dbg.print("--\n", .{}, @src());
+        //     _ = try self.visit(stmt);
+        // }
+        return Result{ .integer = .{ .val = 42 } }; // FIXME: dummy return placeholder
+    }
+
+    // fn visitAssign(self: *Self, node: *const Node) !Result {
+    //     return Result{ .integer = .{ .val = 42 } }; // FIXME: dummy return placeholder
+    // }
 
     fn visit(self: *Self, node: *const Node) anyerror!Result {
         dbg.print("\n", .{}, @src());
@@ -46,7 +101,7 @@ pub const Interpreter = struct {
         }
     }
 
-    fn computerIntBinOp(self: *Self, binop: *const BinOp, lhs_result: Result, rhs_result: Result) !i64 {
+    fn computeIntBinOp(self: *Self, binop: *const BinOp, lhs_result: Result, rhs_result: Result) !i64 {
         const lhs_val = lhs_result.integer.val;
         const rhs_val = rhs_result.integer.val;
         dbg.print("{} {s} {}\n", .{ lhs_val, binop.token.lexeme.?, rhs_val }, @src());
@@ -57,6 +112,13 @@ pub const Interpreter = struct {
             TokenType.mul => new_val = lhs_result.integer.val * rhs_result.integer.val,
             TokenType.div => new_val = @divTrunc(lhs_result.integer.val, rhs_result.integer.val),
             TokenType.mod => new_val = @mod(lhs_result.integer.val, rhs_result.integer.val),
+
+            TokenType.lt => new_val = @intFromBool(lhs_result.integer.val < rhs_result.integer.val),
+            TokenType.le => new_val = @intFromBool(lhs_result.integer.val <= rhs_result.integer.val),
+            TokenType.eq => new_val = @intFromBool(lhs_result.integer.val == rhs_result.integer.val),
+            TokenType.ge => new_val = @intFromBool(lhs_result.integer.val >= rhs_result.integer.val),
+            TokenType.gt => new_val = @intFromBool(lhs_result.integer.val > rhs_result.integer.val),
+
             else => return NotImplented,
         }
         _ = self;
@@ -64,8 +126,20 @@ pub const Interpreter = struct {
         return new_val;
     }
 
+    fn visitAssignment(self: *Self, binop: *const BinOp) !void {
+        var locals = self.stack.getLast().locals;
+        const id = try self.arena.allocator().dupe(u8, binop.*.lhs.variable.id);
+        const rhs_result = try self.visit(binop.rhs);
+        try locals.put(id, rhs_result);
+    }
+
     fn visitBinOp(self: *Self, binop: *const BinOp) !Result {
         dbg.print("{s}\n", .{binop.token.lexeme.?}, @src());
+        if (binop.token.type == TokenType.assign) {
+            // try self.visitAssignment(binop);
+            return Result{ .void = .{} };
+        }
+
         const lhs_result = try self.visit(binop.lhs);
         const rhs_result = try self.visit(binop.rhs);
         const lhs_res_tag = activeTag(lhs_result);
@@ -75,7 +149,7 @@ pub const Interpreter = struct {
         }
         switch (lhs_res_tag) {
             .integer => {
-                return Result{ .integer = .{ .val = try self.computerIntBinOp(binop, lhs_result, rhs_result) } };
+                return Result{ .integer = .{ .val = try self.computeIntBinOp(binop, lhs_result, rhs_result) } };
             },
             else => return NotImplented, // NOTE: e.g. concat strings
         }
@@ -98,27 +172,26 @@ pub const Interpreter = struct {
     }
 
     pub fn interpret(self: *Self) !u8 {
+        dbg.print("\n", .{}, @src());
         const functions = self.ast.functions;
-        var func_ids_hashset = std.StringHashMap(void).init(self.arena.allocator());
+        var global_funcs = std.StringHashMap(*AstNode.FunctionDecl).init(self.arena.allocator());
         for (functions.items) |func| {
             switch (func.*) {
                 .func_decl => {
                     const id = func.func_decl.*.id;
                     dbg.print("Function id={s}\n", .{id}, @src());
-                    if (func_ids_hashset.contains(id)) {
+                    if (global_funcs.contains(id)) {
                         return Error.DuplicateFunctionDeclaration;
                     }
-                    try func_ids_hashset.put(id, {});
+                    try global_funcs.put(id, func.func_decl);
                 },
                 else => return Error.InterpretError,
             }
         }
-        if (!func_ids_hashset.contains("main")) {
-            return Error.MissingMainFunctionDeclaration;
-        }
         dbg.print("global_len: {} ---\n", .{self.ast.global_statements.items.len}, @src());
         dbg.print("funcs_len: {} ---\n", .{self.ast.functions.items.len}, @src());
-        _ = try self.visit(self.ast.functions.items[0].*.func_decl.statements.items[0]);
+        _ = try self.visitFuncDecl(global_funcs.get("main") orelse return Error.MissingMainFunctionDeclaration);
+        // _ = try self.visit(self.ast.functions.items[0].*.func_decl.statements.items[0]);
         // TODO: implement program return value
         return 0;
     }
